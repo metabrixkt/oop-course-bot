8af8bfc7e4f800016e749a77f7cacc6058b0d67c
Ilushka-Ilushka <148797990+Ilushka-Ilushka@users.noreply.github.com>
2024-10-21 16:29:55 +0500

    add start-command (#2)

---
+++ src/main/java/dev/metabrix/urfu/oopbot/commands/StartCommand.java
@@ -0,0 +1,46 @@
+package dev.metabrix.urfu.oopbot.commands;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.jetbrains.annotations.NotNull;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import dev.metabrix.urfu.oopbot.BotCommand;
+import dev.metabrix.urfu.oopbot.util.Emoji;
+import dev.metabrix.urfu.oopbot.util.command.CommandContext;
+import dev.metabrix.urfu.oopbot.util.command.CommandExecutionResult;
+import dev.metabrix.urfu.oopbot.util.command.CommandHandler;
+
+public class StartCommand implements CommandHandler {
+    @Override
+    public @NotNull CommandExecutionResult execute(@NotNull CommandContext ctx) {
+        try {
+            ctx.getApplication().getBot().execute(SendMessage.builder()
+                .chatId(ctx.getChat().getId())
+                .parseMode("html")
+                .text(
+                    """
+                    %s <b>Трекер задач</b>
+                    
+                    С помощью этого бота можно удобно отслеживать задачи, дедлайны, а также настроить уведомления.
+                    
+                    Список команд: %s
+                    
+                    Бот находится в разработке %s
+                    """.formatted(
+                        Emoji.NOTEBOOK,
+                        Arrays.stream(BotCommand.values())
+                            .map(botCommand -> "/" + botCommand.getName())
+                            .collect(Collectors.joining(", ")),
+                        Emoji.EYES
+                    )
+                )
+                .build());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        return CommandExecutionResult.SUCCESS;
+    }
+}
