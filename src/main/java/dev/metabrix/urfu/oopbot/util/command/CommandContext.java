1c1a52a4db046cfb0305b0a5e5587fab94e877cf
metabrixkt <admin@epserv.ru>
2024-12-01 00:32:14 +0500

    commands: Make data-related API available to commands via CommandContext

--- src/main/java/dev/metabrix/urfu/oopbot/util/command/CommandContext.java
+++ src/main/java/dev/metabrix/urfu/oopbot/util/command/CommandContext.java
@@ -1,11 +1,14 @@
 package dev.metabrix.urfu.oopbot.util.command;
 
 import dev.metabrix.urfu.oopbot.BotApplication;
+import dev.metabrix.urfu.oopbot.storage.DataStorage;
+import dev.metabrix.urfu.oopbot.storage.model.Chat;
+import dev.metabrix.urfu.oopbot.storage.model.User;
+import java.util.NoSuchElementException;
 import org.jetbrains.annotations.NotNull;
-import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.jetbrains.annotations.Nullable;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 
 /**
  * Контекст исполнения команды.
@@ -22,6 +25,17 @@ public interface CommandContext {
      * @author metabrix
      */
     @NotNull BotApplication getApplication();
+
+    /**
+     * Возвращает хранилище данных.
+     *
+     * @return хранилище данных
+     * @since 1.1.0
+     * @author metabrix
+     */
+    default @NotNull DataStorage getStorage() {
+        return this.getApplication().getStorage();
+    }
 
     /**
      * Возвращает объект события Telegram API.
@@ -44,25 +58,82 @@ public interface CommandContext {
     }
 
     /**
-     * Возвращает отправителя команды.
-     *
-     * @return отправитель
-     * @since 1.0.0
-     * @author metabrix
-     */
-    default @NotNull User getSender() {
-        return this.getMessage().getFrom();
-    }
-
-    /**
-     * Возвращает чат, в котором была вызвана команда.
+     * Возвращает чат, в котором была вызвана команда, в виде объекта Telegram API.
      *
      * @return чат
      * @since 1.0.0
      * @author metabrix
      */
-    default @NotNull Chat getChat() {
+    default @NotNull org.telegram.telegrambots.meta.api.objects.Chat getTelegramChat() {
         return this.getMessage().getChat();
+    }
+
+    /**
+     * Возвращает отправителя команды в виде объекта Telegram API.
+     *
+     * @return отправитель
+     * @since 1.0.0
+     * @author metabrix
+     */
+    default @NotNull org.telegram.telegrambots.meta.api.objects.User getTelegramSender() {
+        return this.getMessage().getFrom();
+    }
+
+    /**
+     * Возвращает чат, в котором была вызвана команда, в виде объекта {@link Chat}, если он есть в хранилище данных.
+     *
+     * @return чат или {@code null}, если его нет в хранилище данных
+     * @since 1.1.0
+     * @author metabrix
+     */
+    default @Nullable Chat getChatIfExists() {
+        org.telegram.telegrambots.meta.api.objects.Chat telegramChat = this.getTelegramChat();
+        return this.getStorage().chats().getByTelegramId(telegramChat.getId());
+    }
+
+    /**
+     * Возвращает чат, в котором была вызвана команда, в виде объекта {@link Chat}.
+     * <p>
+     * Этот метод ожидает, что чат был добавлен в хранилище данных до передачи управления в обработчик команды.
+     *
+     * @return чат
+     * @throws NoSuchElementException если чата нет в хранилище данных
+     * @since 1.1.0
+     * @author metabrix
+     */
+    default @NotNull Chat getChat() {
+        Chat chat = this.getChatIfExists();
+        if (chat == null) throw new NoSuchElementException("Chat " + this.getTelegramChat() + " not found in data storage");
+        return chat;
+    }
+
+    /**
+     * Возвращает отправителя команды в виде объекта {@link User}, если он есть в хранилище данных.
+     *
+     * @return пользователь или {@code null}, если его нет в хранилище данных
+     * @since 1.1.0
+     * @author metabrix
+     */
+    default @Nullable User getSenderIfExists() {
+        org.telegram.telegrambots.meta.api.objects.User telegramUser = this.getTelegramSender();
+        return this.getStorage().users().getByTelegramId(telegramUser.getId());
+    }
+
+    /**
+     * Возвращает отправителя команды в виде объекта {@link User}.
+     * <p>
+     * Этот метод ожидает, что пользователь был добавлен в хранилище данных до передачи
+     * управления в обработчик команды.
+     *
+     * @return пользователь
+     * @throws NoSuchElementException если пользователя нет в хранилище данных
+     * @since 1.1.0
+     * @author metabrix
+     */
+    default @NotNull User getSender() {
+        User user = this.getSenderIfExists();
+        if (user == null) throw new NoSuchElementException("User " + this.getTelegramSender() + " not found in data storage");
+        return user;
     }
 
     /**
