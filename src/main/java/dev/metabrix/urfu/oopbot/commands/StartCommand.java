b7f42bffe473213e95e258947f232995995d32c1
metabrixkt <admin@epserv.ru>
2024-11-30 22:45:46 +0500

    commands: Use MarkdownV2 formatting, optimize imports

--- src/main/java/dev/metabrix/urfu/oopbot/commands/StartCommand.java
+++ src/main/java/dev/metabrix/urfu/oopbot/commands/StartCommand.java
@@ -1,17 +1,16 @@
 package dev.metabrix.urfu.oopbot.commands;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import org.jetbrains.annotations.NotNull;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import dev.metabrix.urfu.oopbot.BotCommand;
 import dev.metabrix.urfu.oopbot.util.Emoji;
 import dev.metabrix.urfu.oopbot.util.command.CommandContext;
 import dev.metabrix.urfu.oopbot.util.command.CommandExecutionResult;
 import dev.metabrix.urfu.oopbot.util.command.CommandHandler;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class StartCommand implements CommandHandler {
     @Override
@@ -19,18 +18,18 @@ public class StartCommand implements CommandHandler {
         try {
             ctx.getApplication().getBot().execute(SendMessage.builder()
                 .chatId(ctx.getChat().getId())
-                .parseMode("html")
+                .parseMode(ParseMode.MARKDOWNV2)
                 .text(
                     """
                     %s
                     
-                    С помощью этого бота можно удобно отслеживать задачи, дедлайны, а также настроить уведомления.
+                    С помощью этого бота можно удобно отслеживать задачи, дедлайны, а также настроить уведомления\\.
                     
                     Список команд: %s
                     
                     Бот находится в разработке %s
                     """.formatted(
-                        ctx.getChat().isUserChat() ? Emoji.NOTEBOOK + " <b>Трекер задач</b>" : "",
+                        ctx.getChat().isUserChat() ? Emoji.NOTEBOOK + " *Трекер задач*" : "",
                         Arrays.stream(BotCommand.values())
                             .map(botCommand -> "/" + botCommand.getName())
                             .collect(Collectors.joining(", ")),
