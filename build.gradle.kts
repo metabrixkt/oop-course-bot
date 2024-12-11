756039cc0852c1f6fa360b0ceb47d0198139480a
metabrixkt <admin@epserv.ru>
2024-12-11 15:14:15 +0500

    build: Update build & runtime dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCach
 plugins {
     java
     application
-    id("com.gradleup.shadow") version "8.3.2"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "dev.metabrix.urfu"
@@ -14,21 +14,21 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:25.0.0")
+    implementation("org.jetbrains:annotations:26.0.1")
 
     implementation("com.typesafe:config:1.4.3")
     implementation("org.json:json:20240303")
 
     implementation("org.slf4j:slf4j-api:2.0.16")
-    implementation("org.apache.logging.log4j:log4j-core:2.24.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.2")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.2")
     implementation("net.minecrell:terminalconsoleappender:1.3.0")
 
     implementation("org.telegram:telegrambots:6.9.7.1")
 
     implementation("com.mysql:mysql-connector-j:9.1.0")
 
-    testImplementation(platform("org.junit:junit-bom:5.11.1"))
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
